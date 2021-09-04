8044d0a9c13812cf0b8e9de56423425aca596f29
Fegaso <felipe.gausmannsocolowski@gmail.com>
2021-09-04 16:49:12 -0300

    Update to the site 2

--- site/script.js
+++ site/script.js
@@ -1,7 +1,16 @@
 function greetAction() {
     let name = prompt(`What's your name?`);
 
-    alert(`Hi ${name}, glad to see you!`);
+    alert(`Hey ${name}, thanks for accessing this site!`);
 }
 
 document.querySelector("#greet").addEventListener('click', greetAction);
+
+let counter = 0;
+
+function addCounter() {
+    counter++;
+    document.querySelector("#counter").innerHTML = counter;
+}
+
+document.querySelector("#adder").addEventListener('click', addCounter);
